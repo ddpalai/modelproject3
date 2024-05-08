42a75937598b727064af135b4cb165cd9207a407
ddpalai <111751263+ddpalai@users.noreply.github.com>
2024-05-08 23:09:31 +0530

    First commit by Debabrata

---
+++ testapp/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from testapp.models import Student
+# Create your views here.
+def student_view(request):
+    #student_list = Student.objects.all()
+    #student_list = Student.objects.filter(marks__lt=35)
+    #student_list = Student.objects.filter(name__startswith='A')
+    student_list = Student.objects.all().order_by('-marks')
+    my_dict = {'student_list':student_list}
+    return render(request,'testapp/std.html',my_dict)
